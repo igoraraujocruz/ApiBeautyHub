70d0fff24f5f5c08915957bff36d31103f4e64f0
gabriel-r-barreto <58337171+gabriel-r-barreto@users.noreply.github.com>
2023-05-03 17:27:10 -0300

    atualizacao 2

--- server.js
+++ server.js
@@ -81,6 +81,77 @@ app.post('/register', async (req, res) => {
 
 });
 
+
+
+// Rota para registrar uma nova reserva de lab
+app.post('/register/booking/classroom', async (req, res) => {
+  var { nome, dia } = req.body;
+  // console.log(req.body)
+  if (!nome || !dia) {
+    return res.status(400).json({ message: 'Preencha todos os campos.' });
+  }
+
+  dia = dia.split('/').reverse().join('-');
+
+  // console.log(dia);
+
+  try {
+    // Verifica se o email já está em uso
+    const [rows, fields] = await pool.query('SELECT * FROM rooms WHERE nome = "'+ nome + '" ORDER BY dia DESC');
+    
+    if (rows.length > 0) {
+      console.log(rows)
+    const date1 = new Date(rows[0].dia);
+    const date2 = new Date(dia);
+
+    const diffTime = Math.abs(date2 - date1);
+    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+    console.log("diferenca em dias", diffDays)
+
+    if (diffDays < 15) {
+      diffDays = 0
+      return res.status(400).json({ message: 'Sua ultima reserva foi feita em menos de 15 dias.' });
+
+    }
+
+    if (diffDays > 31) {
+      diffDays = 0
+      return res.status(400).json({ message: 'Data da reserva muito antiga.' });
+    }
+
+  }
+
+    // Insere o novo usuário no banco de dados
+    await pool.query('INSERT INTO rooms (nome, sala, dia) VALUES (?, ?, ?)', [nome,'Laboratório 2',dia]);
+
+    return res.status(201).json({ message: 'Reserva feita com sucesso.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erro ao realizar a reserva.' });
+  }
+});
+
+
+
+// Rota para buscar as reservas
+app.get('/buscar/booking/classroom', async (req, res) => {
+  try {
+    // Verifica se o email já está em uso
+    const [rows, fields] = await pool.query('SELECT * FROM rooms');
+    
+    if (rows.length > 0) {
+      console.log(rows)
+
+  }
+
+    return res.status(200).json({ data: rows });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erro ao obter as reservas.' });
+  }
+});
+
 const server = http.createServer(app);
 
 const io = socketIo(server, {
