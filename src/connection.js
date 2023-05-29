8ecc6aaa754a0644d9bf8cfdd40ea1496096a39e
Igor <igoraraujocruzz@gmail.com>
2023-05-29 00:25:18 -0300

    changes

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
 export const connection = createPool({
-  host: 'containers-us-west-59.railway.app',
-  user: 'root',
-  password: 'raPQPHx09aDIg1uXhox4',
-  database: 'railway',
-  port: 8051,
+  host: 'localhost',
+  user: 'beautyHubUser',
+  password: 'beautyHub123',
+  database: 'beautyHubDb',
+  port: 3306,
   waitForConnections: true,
 });
